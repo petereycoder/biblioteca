7a4ce86017b968177dc1fff575fa6ffbaaaf94bd
petereycoder <fp.pipe.reyes@gmail.com>
2024-08-26 17:26:16 -0600

    Se crearon archivos necesarios para el CRUD

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=biblioteca',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Admin',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
