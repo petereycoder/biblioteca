4e13c6a65b9269d00e4b029abc4f927fdeb539fc
petereycoder <fp.pipe.reyes@gmail.com>
2024-08-26 16:49:34 -0600

    Se creó un formulario para la suma de dos números

---
+++ views/sitio/inicio.php
@@ -0,0 +1,24 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<?php
+if ($mensaje) {
+    echo Html::tag('div', Html::encode($mensaje), ['class' => 'alert alert-success']);
+} 
+?>
+
+Hola Usuario
+
+<?php $formulario = ActiveForm::begin(); ?>
+
+<?= $formulario->field($model, 'valora') ?>
+
+<?= $formulario->field($model, 'valorb') ?>
+
+<div class="form-group">
+<?= Html::submitButton('Enviar', ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?php ActiveForm::end() ?>
