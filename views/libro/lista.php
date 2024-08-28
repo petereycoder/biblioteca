b0b6adbdd75a325d7de55fbbf06ac00795af1e65
petereycoder <fp.pipe.reyes@gmail.com>
2024-08-28 10:45:50 -0600

    Se creó de la lista

--- views/libro/lista.php
+++ views/libro/lista.php
@@ -7,4 +7,18 @@ use yii\widgets\LinkPager;
 <h1>Libros a la venta</h1>
 <div class="row">
 
+    <?php foreach($libros as $libro): ?>
+
+        <div class="col-4 col-sm-12  col-md-4 m-2  img-thumbnail ">
+            <a href="#" class="img-fluid">
+                <?= Html::img($libro->imagen) ?>
+                <?= Html::encode("{$libro->titulo}") ?>
+            </a>
+        </div>
+
+        
+    <?php endforeach; ?>
+
 </div>
+
+<?= LinkPager::widget(['pagination' => $paginacion]) ?>
